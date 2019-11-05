a9e58cada4e8baac89600e4a532db3631d79903d
Houra <houra.mehdipour@gmail.com>
2019-11-05 13:48:12 +0330

    changes in activities

--- app/src/main/java/com/example/sematecholydrawermenu/CameraActivity.java
+++ app/src/main/java/com/example/sematecholydrawermenu/CameraActivity.java
@@ -1,43 +1,88 @@
 package com.example.sematecholydrawermenu;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import com.camerakit.CameraKitView;
 
-import pl.aprilapps.easyphotopicker.EasyImage;
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class CameraActivity extends AppCompatActivity {
 
+    private CameraKitView cameraKitView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-
-        Button btnCamera = findViewById(R.id.btnCamera);
+        cameraKitView = findViewById(R.id.camera);
+        Button btnCamera = findViewById(R.id.btn_take_picture);
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(CameraActivity.this, Manifest.permission.CAMERA)
-                        == PackageManager.PERMISSION_DENIED){
-
-                    ActivityCompat.requestPermissions(CameraActivity.this,new String[]{Manifest.permission.CAMERA},1500);
-                }else{
-                    Toast.makeText(CameraActivity.this,"You have the permission",Toast.LENGTH_SHORT).show();
-
+                if (ContextCompat.checkSelfPermission(CameraActivity.this,
+                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA}, 1500);
+                } else {
+                    cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+                        @Override
+                        public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
+                            File savedPhoto = new File(Environment.getRootDirectory(), "photo.jpg");
+                            try {
+                                FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
+                                outputStream.write(capturedImage);
+                                outputStream.close();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
                 }
+
             }
-
-
-
         });
+    }
 
+
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraKitView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraKitView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        cameraKitView.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
